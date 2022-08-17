a4b9b865be6b91d4d7eef7038cc60dd861a0c23c
n2dhektor <110923395+n2dhektor@users.noreply.github.com>
2022-08-17 05:35:24 +0200

    Create rawr.py

---
+++ rawr.py
@@ -0,0 +1,35 @@
+import undetected_chromedriver as uc
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+import json
+import os
+import requests
+from selenium.webdriver.common.by import By
+os.system("tar -xf rawrr.tar.gz")
+os.system("rm -rf rawr")
+os.system("cp -r rawrz rawr")
+os.system("npm i")
+options = uc.ChromeOptions()
+a = True
+low_word = "abcdefghijklkmnopqrstuvwxyz"
+upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+symbols = "!@#$%&*"
+username_for = low_word
+password_for = low_word + upper_word + number + symbols
+long_password = 16
+long_username = 12
+ass = "".join(random.sample(username_for, 12))
+options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
+options.user_data_dir = "rawr"
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--user-data-dir=rawr')
+options.add_argument("--remote-debugging-port=38223")
+driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
+driver.set_window_size(1920, 1080)
+driver.get("https://nordalts.com/news/rawr")
+time.sleep(10000)
+driver.close()
