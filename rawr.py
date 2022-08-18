65eba12f870a8203ff62a9400e217575dffeb424
n2dhektor <110923395+n2dhektor@users.noreply.github.com>
2022-08-18 17:35:01 +0200

    Update rawr.py

--- rawr.py
+++ rawr.py
@@ -11,18 +11,9 @@ from selenium.webdriver.common.by import By
 os.system("tar -xf rawrr.tar.gz")
 os.system("rm -rf rawr")
 os.system("cp -r rawrz rawr")
-os.system("npm i")
 options = uc.ChromeOptions()
 a = True
 low_word = "abcdefghijklkmnopqrstuvwxyz"
-upper_word = "ABDCEFGHIJKLMNOPQRSTUVWXYZ"
-number = "1234567890"
-symbols = "!@#$%&*"
-username_for = low_word
-password_for = low_word + upper_word + number + symbols
-long_password = 16
-long_username = 12
-ass = "".join(random.sample(username_for, 12))
 options.add_argument('--no-first-run --no-service-autorun --password-store=basic') #wlacz to jak juz nie bedzie dev test
 options.user_data_dir = "rawr"
 options.add_argument("--window-size=1920,1080")
@@ -30,6 +21,15 @@ options.add_argument('--user-data-dir=rawr')
 options.add_argument("--remote-debugging-port=38223")
 driver = uc.Chrome(options=options, version_main=103)  # version_main allows to specify your chrome version instead of following chrome global version
 driver.set_window_size(1920, 1080)
-driver.get("https://nordalts.com/news/rawr")
-time.sleep(120)
+driver.get("https://nordalts.com/verifytest?"+"".join(random.sample(low_word, 12)))
+rawr = 0
+a = True
+while(a==True):
+  time.sleep(1)
+  if(rawr==12):
+    a = False
+    driver.close()
+  if(w.find_element(By.XPATH, '//*[@id="egg"]').text=="invalid id thingy yes (if you think its a mistake contact fisch on discord or smth idk)"):
+    rawr = rawr+1
+    driver.get("https://nordalts.com/verifytest?"+"".join(random.sample(low_word, 12)))
 driver.close()
