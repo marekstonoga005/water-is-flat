ff93b78d219606c88456be9899e64195b0e6ac1f
n2dhektor <110923395+n2dhektor@users.noreply.github.com>
2022-09-11 14:11:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ setInterval(()=>{
 fetch("https://theddosgame.com/authenticate.php", {
     "credentials": "include",
     "headers": {
-        "User-Agent": "sudfhdsjcnkxz",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
         "Content-Type": "application/x-www-form-urlencoded",
