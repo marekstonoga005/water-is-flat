478dd052af5efe8af477f4fb0b6b67408e7f3fc7
n2dhektor <110923395+n2dhektor@users.noreply.github.com>
2022-09-23 19:44:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def rawrzzzz():
     time.sleep(1)
     driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div[1]/button").click()
     time.sleep(33)
-  except as rawrzxx:
+  except Exception as rawrzxx:
     print(rawrzxx)
     pass
     if(xxxtime<int(time.time())):
