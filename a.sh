204ab81fe40e1ebd550c48e2886239a0298cbacf
n2dhektor <110923395+n2dhektor@users.noreply.github.com>
2022-09-24 14:42:28 +0200

    Update a.sh

--- a.sh
+++ a.sh
@@ -2,20 +2,8 @@ rm -rf rawrz
 tar -xf volx.tar.gz
 rm -rf rawr1
 rm -rf rawr2
-rm -rf rawr3
-rm -rf rawr4
-rm -rf rawr5
 cp -r rawrz rawr1
 cp -r rawrz rawr2
-cp -r rawrz rawr3
-cp -r rawrz rawr4
-cp -r rawrz rawr5
 python3 main1.py &
 sleep 8
-python3 main2.py &
-sleep 8
-python3 main3.py &
-sleep 8
-python3 main4.py &
-sleep 8
-python3 main5.py
+python3 main2.py
