c7ff1475d8131f547dfc118aaa593a163a13d8b1
n2dhektor <110923395+n2dhektor@users.noreply.github.com>
2022-10-17 19:44:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 if __name__ == "__main__":
+    import os
     import undetected_chromedriver as uc
     import time
     from selenium.webdriver.common.by import By
     from selenium.webdriver.common.action_chains import ActionChains
     import random
     options = uc.ChromeOptions()
+    options.user_data_dir = os.getcwd()+"/fuck"
     driver = uc.Chrome(options=options, version_main=106)  # version_main allows to specify your chrome version instead of following chrome global version
     driver.set_window_size(1920, 1080)
     low_word = "abcdefghijklkmnopqrstuvwxyz"
+    input("x")
     driver.execute_script('''window.open("http://bings.com","_blank");''')
     driver.switch_to.window(driver.window_handles[1])
-    time.sleep(1)
-    driver.get("https://mail.tm/")
-    time.sleep(5)
+    driver.get("https://www.emailnator.com/")
+    time.sleep(6)
     try:
-        driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/button[3]').click()
+        driver.find_element(By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]').click()
     except:
         pass
+    time.sleep(0.5)
+    try:
+        driver.find_element(By.XPATH, '//*[@id="close-btn-ann"]').click()
+    except:
+        pass
+    time.sleep(1.5)
+    driver.find_element(By.XPATH, '//*[@id="custom-switch-domain"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//*[@id="custom-switch-dotGmail"]').click()
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '/html/body/div/div/main/div[1]/div/div/div/div[2]/div/div[5]/div/button').click()
+    time.sleep(2)
+    emil = driver.find_element(By.XPATH, '/html/body/div/div/main/div[1]/div/div/div/div[2]/div/div[1]/input').get_attribute("value")
+    emil = emil.split("+")[0]+"+"+"".join(random.sample(low_word, 13))+"@gmail.com"
     time.sleep(1)
-    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[2]/div[5]/button').click()
-    time.sleep(6)
-    emil = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[1]/div/div/input').get_attribute("value")
-    print(emil)
     driver.switch_to.window(driver.window_handles[0])
+    # random.choice(a)+"+"+emailrepl+"@gmail.com"
+    print(emil)
 
     driver.get("https://deepnote.com/sign-up")
     time.sleep(1)
@@ -31,18 +45,23 @@ if __name__ == "__main__":
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[2]/div/div/div/input').send_keys(emil)
     time.sleep(1)
     driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div/div[2]/button").click()
-    time.sleep(20)
+    time.sleep(40)
+    
+    
     driver.switch_to.window(driver.window_handles[1])
-    driver.get(driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div[2]/ul/li/a").get_attribute("href"))
-    time.sleep(3)
-    iframe = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div[3]/div[2]/div/iframe")
-    driver.switch_to.frame(iframe)
-    time.sleep(1)
-    linkz = driver.find_element(By.XPATH, "/html/body/table[2]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td/a").get_attribute("href")
-    driver.switch_to.default_content()
-    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(2)
+    driver.get("https://www.emailnator.com/inbox/"+emil)
+    # driver.find_element(By.XPATH, "/html/body/div/div/main/div[1]/div/div/div/div[2]/div/div[3]/button").click()
+    time.sleep(6)
+    linkzxd = driver.find_element(By.XPATH, "/html/body/div/div/section/div/div/div[3]/div/div[2]/div[2]/div/table/tbody/tr[2]/td/a").get_attribute("href")
     time.sleep(0.5)
-    driver.get(linkz)
+    driver.get(linkzxd)
+    time.sleep(7)
+    verifylink = driver.find_element(By.XPATH, "/html/body/div/div/section/div/div/div[3]/div/div/div[2]/div/div/table[2]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td/a").get_attribute("href")
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(1)
+    driver.get(verifylink)
+    time.sleep(0.5)
     time.sleep(3)
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/div[1]/input[1]').send_keys("".join(random.sample(low_word, 6)))
     time.sleep(0.5)
@@ -168,6 +187,3 @@ if __name__ == "__main__":
     time.sleep(1)
     driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div[2]/div/div[2]/div/div[1]/div/div/div[4]/div/div[2]/div[3]/div/div[1]").click()
     time.sleep(14)
-
-
-
